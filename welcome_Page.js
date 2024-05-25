81a55eea4dd8a021b8944ba04d5e318af5468a69
rajnish98 <rajnishpandey63@gmail.com>
2024-05-25 17:11:25 +0530

    dyanamic welcome message

---
+++ welcome_Page.js
@@ -0,0 +1,16 @@
+const output = document.querySelector('.output');
+const userVal = document.querySelector('input[name="userName"]');
+const btn = document.querySelector('button');
+userVal.style.borderColor = '#333333';
+btn.onclick = () => {
+  if (userVal.value.length > 3) {
+    const message = `welcome to the site ${userVal.value}`;
+    output.textContent = message;
+    userVal.style.display = 'none';
+    btn.style.display = 'none';
+  }
+  else {
+    output.textContent = 'Name length must be 3+ characters';
+    userVal.style.borderColor = 'red';
+  }
+}
